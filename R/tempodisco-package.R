5547a9291145c2b092a75d5f0818cea64bfe5319
Isaac Kinley <isaac.kinley@gmail.com>
2024-11-08 17:07:52 -0500

    Resolve conflict

--- R/tempodisco-package.R
+++ R/tempodisco-package.R
@@ -1,6 +1,6 @@
 
-#' @importFrom stats optim predict qlogis residuals integrate coef BIC AIC glm binomial fitted approx predict.glm na.omit filter
-#' @importFrom graphics lines points title
+#' @importFrom stats optim predict qlogis plogis residuals integrate coef BIC AIC glm binomial fitted approx predict.glm na.omit median filter
+#' @importFrom graphics lines points title axis
 #' @importFrom methods is
 #' @importFrom grDevices rgb
 NULL
